f33e63ae602c36c07c1785a7477ef5fc89298234
Miguel Salvador <miguelrubiorubio30@gmail.com>
2019-03-22 20:58:35 +0100

    LOGIN BACKEND terminado menos la seguridad. Comienzo de la gestion de usuarios

--- PHP/funciones.php
+++ PHP/funciones.php
@@ -1,20 +1,61 @@
 <?php
+	//FUNCIÓN PARA CONECTARSE A LA BD DE LA APLICACIÓN
 	function conexion_database() {
 		$conexion = new mysqli("localhost", "root", "", "inventario");
 		return $conexion;
 	}
+	//
 	function validar_login($email, $password) {
 		$conexion = conexion_database();
-		$sentencia = $conexion->prepare("SELECT password FROM usuarios WHERE LOWER(email) = LOWER(?)");
+		$sentencia = $conexion->prepare("SELECT password, tipo FROM usuarios WHERE LOWER(email) = LOWER(?)");
 		$sentencia->bind_param("s", $email);
 		$sentencia->execute();
 		$resultado = $sentencia->get_result();
 		if ($resultado->num_rows == 0) return "<p id='fallo'>El usuario no existe</p>";
 		$resultado = $resultado->fetch_assoc();
 		if ($resultado['password'] != md5($password)) return "<p id='fallo'>La contraseña no es correcta</p>";
-		else return True;
+		else return array('tipo' => $resultado['tipo']);
 	}
 	function cerrar_sesion() {
 		session_destroy();
+		if (isset($_COOKIE['email'])) setcookie("email", "", time() - 1, "/");
+		if (isset($_COOKIE['password'])) setcookie("password", "", time() - 1, "/");
+		header('Location: /PHP/index.php');
+	}
+
+	function ver_usuarios() {
+		$conexion = conexion_database();
+		$resultado = $conexion->query("SELECT email, tipo FROM usuarios");
+		if (is_object($resultado) and $resultado->num_rows > 0) {
+			echo "<pre>";
+			while ($fila = $resultado->fetch_assoc()) {
+				$usuarios[] = $fila;
+			}
+			return $usuarios;
+		}
+		else echo "la consulta ha fallado o no hay usuarios que mostrar";
+	}
+	function crear_usuario() {
+		
+	}
+
+	function borrar_usuario($email) {
+		$conexion = conexion_database();
+		$sentencia = $conexion->prepare("DELETE FROM usuarios WHERE LOWER(email) = LOWER(?)");
+		$sentencia->bind_param("s", $email);
+		if (!$sentencia->execute())	return False;
+		else return True;
+	}
+
+	function registrar_login($fecha, $email, $descripcion) {
+		$conexion = conexion_database();
+		$evento = "login";
+		$sentencia = $conexion->prepare("INSERT INTO logs VALUES (?, ?, ?, ?)");
+		$sentencia->bind_param("isss", $fecha, $email, $descripcion, $evento);
+		$sentencia->execute();
+	}
+
+	function crear_localizacion() {
+
 	}
 ?>
--- PHP/index.php
+++ PHP/index.php
@@ -1,15 +1,50 @@
 <?php
 	session_start();
 	include "funciones.php";
-	if (isset($_SESSION["email"]) and isset($_SESSION["password"]))	header('Location: menu.php');
-	elseif (isset($_POST['campo_enviar'])) {
-		echo $validacion_login = validar_login($_POST['campo_email'], $_POST['campo_password']);
-		if ($validacion_login === True) {
-			$_SESSION["email"] = $_POST['campo_email'];
-			$_SESSION["password"] = $_POST['campo_password'];
-			header('Location: menu.php');
+	/* *** INICIO DE BLOQUE COMÚN A TODAS LAS PÁGINAS WEB DE LA APLICACIÓN PARA CONTROLAR EL INICIO DE SESIÓN. 
+	EN ESTA PÁGINA DIFIERE PORQUE SI YA SE HA HECHO LOGIN CORRECTO EN LA PÁGINA SE REDIRIGE A OTRO (NO TENDRÍA SENTIDO VOLVER A MOSTRAR EL FORMULARIO DE INICIO),
+	EN EL RESTO SE PERMITIRÁ VER EL CONTENIDO Y EN CASO DE NO HABER HECHO LOGIN SE REDIRIGIRÁ AQUÍ *** */
+
+	/*1- SE COMPRUEBA SI HAY UNA SESIÓN INICIADA. EN CASO AFIRMATIVO SE PERMITE LA VISUALIZACIÓN DE LA PÁGINA 
+		(O REDIRIGIR A /PHP/menu.php EN CASO DE SER /PHP/index.php)*/
+	if (isset($_SESSION['email']) and isset($_SESSION['password']) and isset($_SESSION['tipo'])) header('Location: /PHP/menu.php');
+	
+	/*2- SE COMPRUEBA SI HAY COOKIES ALMACENADAS CON CREDENCIALES, EN TAL CASO SE VALIDAN EN LA DATABASE Y SI SON CORRECTOS 
+		SE CREA SESIÓN Y SE PERMITE LA VISUALIZACIÓN DE LA PÁGINA (O REDIRIGIR A /PHP/menu.php EN CASO DE SER /PHP/index.php)*/
+	elseif (isset($_COOKIE['email']) and isset($_COOKIE['password'])) {
+		$resultado_validacion = validar_login($_COOKIE['email'], $_COOKIE['password']);
+		if (is_array($resultado_validacion)) {
+			$_SESSION['email'] = $_COOKIE['email'];
+			$_SESSION['password'] = $_COOKIE['password'];
+			$_SESSION['tipo'] = $resultado_validacion['tipo'];
+			registrar_login(time(), $_SESSION['email'], "Login realizado correctamente");
+			header('Location: /PHP/menu.php');
+		} else {
+			//SI LAS CREDENCIALES ALMACENADAS NO SON CORRECTAS (HAN CAMBIADO EN LA DATABASE O EL USUARIO YA NO EXISTE), LAS BORRAMOS
+			setcookie("email", "", time() - 1, "/");
+			setcookie("password", "", time() - 1, "/");
+		}
+	/*3- SE COMPRUEBA SI SE HA ENVIADO EL FORMULARIO DE INICIO DE SESIÓN, SI ES INCORRECTO SE MUESTRA ERROR. EN CASO CONTRARIO SE CREA LA SESIÓN Y
+		SE REDIRIGE A /PHP/menu.php*/ 
+	} elseif (isset($_POST['campo_enviar'])) {
+		$resultado_validacion = validar_login($_POST['campo_email'], $_POST['campo_password']);
+		if (is_array($resultado_validacion)) {
+			$_SESSION['email'] = $_POST['campo_email'];
+			$_SESSION['password'] = $_POST['campo_password'];
+			$_SESSION['tipo'] = $resultado_validacion['tipo'];
+			//SI EL USUARIO HA MARCADO "RECORDAR CREDENCIALES", ALMACENAMOS COOKIES CON ELLAS CON UNA DURACIÓN DE 30 DÍAS
+			if (isset($_POST['campo_recordar'])) {
+				setcookie("email", $_POST['campo_email'], time() + 60 * 60 * 24 * 30, "/");
+				setcookie("password", $_POST['campo_password'], time() + 60 * 60 * 24 * 30, "/");
+			}
+			registrar_login(time(), $_SESSION['email'], "Login realizado correctamente");
+			header('Location: /PHP/menu.php');
+		} else {
+			echo "BAD LOGIN";
+			registrar_login(time(), $_POST['campo_email'], "Login fallido. Credenciales erroneas");
 		}
 	}
+	//*** FIN DE BLOQUE ***
 ?>
 <!DOCTYPE html>
 <html>
